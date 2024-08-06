8f5768bdc119a6315a585f8556dafff7f13cc273
Ayamex <125165989+Astrayl3@users.noreply.github.com>
2024-08-06 14:28:58 +0700

    Add files via upload

--- 7.1P/app.js
+++ 7.1P/app.js
@@ -1,18 +1,17 @@
 new Vue({
-    el: '#app',
-    data: {
-      posts: []
-    },
-    created() {
-      this.fetchPosts();
-    },
-    methods: {
-      fetchPosts() {
-        const vm = this;
-        $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data) {
-          vm.posts = data;
-        });
-      }
+  el: '#app',
+  data: {
+    posts: []
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      const vm = this;
+      $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data) {
+        vm.posts = data;
+      });
     }
-  });
-  
+  }
+});
